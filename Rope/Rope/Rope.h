ea121cdc53d170b27a2417d063d21e9aeb530aef
David Young <dyoung@pobox.com>
2019-08-26 20:18:45 -0500

    Xcode added some kind of auto-generated Objective-C compatibility shims. *shrug*

---
+++ Rope/Rope/Rope.h
@@ -0,0 +1,13 @@
+//
+//  Rope.h
+//  Rope
+//
+//  Created by David Young on 19 Aug 19.
+//  Copyright © 2019 White Coral Islands. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Rope : NSObject
+
+@end
---
+++ Rope/Rope/Rope.m
@@ -0,0 +1,13 @@
+//
+//  Rope.m
+//  Rope
+//
+//  Created by David Young on 19 Aug 19.
+//  Copyright © 2019 White Coral Islands. All rights reserved.
+//
+
+#import "Rope.h"
+
+@implementation Rope
+
+@end
