8966ead765ffc59ff82e26b64cd863ed7f0b9976
David Young <dyoung@pobox.com>
2020-03-09 20:44:49 -0500

    Flesh out my RopeTextStorage implementation of NSTextStorage—I'm leaving some diagnostic print()s in here, so sue me.

--- Rope/RopeTextStorage.swift
+++ Rope/RopeTextStorage.swift
@@ -1,27 +1,62 @@
-class RopeTextStorage: NSTextStorage {
+public class RopeTextStorage: NSTextStorage {
 	let rope: Rope<Substring>
-	override init() {
+	let _string: RopeString
+	public override init() {
 		rope = Rope()
+		_string = RopeString(rope: rope)
+		super.init()
+	}
+	public init(content t: Substring.Initializer) {
+		rope = Rope<Substring>(content: t)
+		_string = RopeString(rope: rope)
 		super.init()
 	}
 	required init?(coder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
-	required init?(pasteboardPropertyList propertyList: Any, ofType type: NSPasteboard.PasteboardType) {
-		fatalError("init(pasteboardPropertyList:ofType:) has not been implemented")
+	required init?(pasteboardPropertyList propertyList: Any,
+	   ofType type: NSPasteboard.PasteboardType) {
+		fatalError("init(pasteboardPropertyList:ofType:) " +
+		    "has not been implemented")
 	}
-	override func attributes(at location: Int,
-	    effectiveRange range: NSRangePointer?)
+	override public var string: String {
+		return _string as String
+	}
+	override public func attributes(at location: Int,
+	    effectiveRange _range: NSRangePointer?)
 	    -> [NSAttributedString.Key : Any] {
-		return [:]
-	}
-	override func setAttributes(_ optAttrs: [NSAttributedString.Key : Any]?,
-	    range r: NSRange) {
-		let range = NodeIndex(utf16Offset: r.location)..<NodeIndex(utf16Offset: NSMaxRange(r))
-		guard let attrs = optAttrs else {
-			rope.clearAttributesOnRange(range)
-			return
+		let i = NodeIndex(utf16Offset: location)
+		let (attrs, r) = rope.attributes(at: i)
+		if let range = _range {
+			range.initialize(to: r.utf16NSRange)
 		}
-		rope.setAttributes(attrs, range: range)
+		Swift.print("\(attrs.count) attributes on " +
+		    "[\(r.upperBound.utf16Offset), " +
+		    "\(r.lowerBound.utf16Offset)], \(attrs)")
+		return attrs;
+	}
+	override public func replaceCharacters(in range: NSRange,
+	    with str: String) {
+		beginEditing()
+		// TBD make sure `str` is the most efficient representation
+		// to add to Rope<Substring> ?
+		rope[NodeIndex.utf16Range(range)] = Substring(str)
+		let actions = NSTextStorageEditActions.editedCharacters.union(
+		    .editedAttributes)
+		edited(actions, range: range,
+		    changeInLength: (str as NSString).length - range.length)
+		endEditing()
+	}
+	override public func setAttributes(
+	    _ optAttrs: [NSAttributedString.Key : Any]?, range r: NSRange) {
+		beginEditing()
+		let range = NodeIndex.utf16Range(r)
+		if let attrs = optAttrs  {
+			rope.setAttributes(attrs, range: range)
+		} else {
+			rope.clearAttributesOnRange(range)
+		}
+		edited(.editedAttributes, range: r, changeInLength: 0)
+		endEditing()
 	}
 }
