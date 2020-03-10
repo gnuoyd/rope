8e298963cb0fb4777d4b158e16c5cf0b52c1175f
David Young <dyoung@pobox.com>
2020-03-09 20:47:07 -0500

    I experimented a bit with RopeTextStorage in a Playground before getting frustrated and making the RopeSample app. Here is that Playground.

--- Rope/TextView.playground/Contents.swift
+++ Rope/TextView.playground/Contents.swift
@@ -2,6 +2,7 @@
 
 import AppKit
 import PlaygroundSupport
+import Rope
 /*
 let nibFile = NSNib.Name("MyView")
 var topLevelObjects : NSArray?
@@ -14,9 +15,32 @@ let views = (topLevelObjects as! Array<Any>).filter { $0 is NSTextView }
 PlaygroundPage.current.liveView = views[0] as! NSView
  */
 
-let view = NSTextView(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
+public class TextViewController : NSViewController {
+    public let storage = RopeTextStorage(content: "smack!")
+    let textview: NSTextView = NSTextView(frame: NSRect(x: 0, y: 0, width: 100, height: 100))
+    override public func loadView() {
+        vc.view = textview
+    }
+    override public func viewWillAppear() {
+        // storage.replaceCharacters(in: NSMakeRange(0, 0), with: "smack!")
+        textview.layoutManager!.replaceTextStorage(storage)
+        Swift.print("storage replaced!")
+        if textview.textStorage is RopeTextStorage {
+            Swift.print("properly, even!")
+        }
+        super.viewWillAppear()
+    }
+}
+
+// let ctlr = TextViewController()
+// let view = ctlr.view
+// let storage = RopeTextStorage()
 // view.text = "Boo!"
-view.backgroundColor = .cyan
-PlaygroundPage.current.liveView = view
-view.needsDisplay = true
+// view.backgroundColor = .cyan
+// PlaygroundPage.current.liveView = view
+// view.needsDisplay = true
+
+let vc = TextViewController()
+PlaygroundPage.current.liveView = vc
+
 
