b33b7771b2acb6f19014fbd7f41ff6df6fb9869b
David Young <dyoung@pobox.com>
2021-05-18 20:42:30 -0500

    Extract into its own source file for reuse my custom operator, *, for repeating a String.

--- Rope/Sources/Rope/Node.swift
+++ Rope/Sources/Rope/Node.swift
@@ -20,20 +20,6 @@ extension Dictionary where Key == NSAttributedString.Key, Value == Any {
 	}
 }
 
-func *(_ s: String, _ times: Int) -> String {
-	if times < 0 {
-		return String(s.reversed()) * -times
-	}
-	if times == 0 {
-		return ""
-	}
-	if times.isMultiple(of: 2) {
-		let t = s * (times / 2)
-		return t + t
-	}
-	return s + s * (times - 1)
-}
-
 extension Rope {
 	class ReadonlyExtentController : ExtentController {
 		override func transformingAttributes(
